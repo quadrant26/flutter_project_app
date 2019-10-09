c54e3e131d1f624c9c9feba71b104efd3b492fcb
quadrant26 <quadrant26@163.com>
2019-10-09 16:51:55 +0800

    swiper

--- flutter_trip/lib/pages/home_page.dart
+++ flutter_trip/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -13,12 +14,35 @@ class _HomePageState extends State<HomePage> {
     initialPage: 0,
   );
 
+  List _imageUrls = [
+    'http://b.hiphotos.baidu.com/image/pic/item/0eb30f2442a7d9337119f7dba74bd11372f001e0.jpg',
+    'http://a.hiphotos.baidu.com/image/pic/item/9a504fc2d5628535bdaac29e9aef76c6a6ef63c2.jpg',
+    'http://g.hiphotos.baidu.com/image/pic/item/21a4462309f7905296a7578106f3d7ca7acbd5d0.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       body: Center(
-        child: Text('首页')
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 260,
+              child: Swiper(
+                itemCount: _imageUrls.length,
+                autoplay: true,
+                itemBuilder: (BuildContext context, int index){
+                  return Image.network(
+                    _imageUrls[index],
+                    fit: BoxFit.cover
+                  );
+                },
+                pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        )
       )
     );
   }
