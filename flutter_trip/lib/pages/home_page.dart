b61a3413dab8eb35776b9610c72a06139b186b82
quadrant26 <quadrant26@163.com>
2019-10-09 11:22:16 +0800

    tab nav bottom init page

---
+++ flutter_trip/lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState()  => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text('首页')
+      )
+    );
+  }
+
+}
---
+++ flutter_trip/lib/pages/my_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatefulWidget {
+
+  @override
+  _MyPageState createState()  => _MyPageState();
+
+}
+
+class _MyPageState extends State<MyPage> {
+
+  final PageController _controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text('首页')
+      )
+    );
+  }
+
+}
