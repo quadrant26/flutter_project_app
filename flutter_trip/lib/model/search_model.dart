0f3db824fa7206a0bad69ba21640493073f68be5
quadrant26 <quadrant26@163.com>
2019-10-15 16:57:24 +0800

    search 数据模型

---
+++ flutter_trip/lib/model/search_model.dart
@@ -0,0 +1,58 @@
+
+const URL = 'https://m.ctrip.com/restapi/h5api/searchapp/search?source=mobileweb&action=autocomplete&contentType=json&keyword=';
+
+class SearchModel{
+  final List<SearchItem> data;
+
+  SearchModel({this.data});
+
+  factory SearchModel.fromJson(Map<String, dynamic> json){
+    var dataJson = json['data'] as List;
+    List<SearchItem> data = dataJson.map( (i) => SearchItem.fromJson(i)).toList();
+
+    return SearchModel(data: data);
+  }
+}
+
+
+
+class SearchItem {
+  String word;
+  String type;
+  String districtname;
+  String url;
+  String price;
+  String star;
+  String zonename;
+
+  SearchItem(
+      {this.word,
+        this.type,
+        this.districtname,
+        this.url,
+        this.price,
+        this.star,
+        this.zonename});
+
+  SearchItem.fromJson(Map<String, dynamic> json) {
+    word = json['word'];
+    type = json['type'];
+    districtname = json['districtname'];
+    url = json['url'];
+    price = json['price'];
+    star = json['star'];
+    zonename = json['zonename'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['word'] = this.word;
+    data['type'] = this.type;
+    data['districtname'] = this.districtname;
+    data['url'] = this.url;
+    data['price'] = this.price;
+    data['star'] = this.star;
+    data['zonename'] = this.zonename;
+    return data;
+  }
+}
